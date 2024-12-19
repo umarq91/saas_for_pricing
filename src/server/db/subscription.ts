586f40105d27c2d77c20d7b180ce46e19034fada
umarq91 <m.umarqureshi091@gmail.com>
2024-12-19 13:07:12 +0500

    add clerk webhooks

---
+++ src/server/db/subscription.ts
@@ -0,0 +1,10 @@
+import { db } from "@/drizzle/db";
+import { UserSubscriptionTable } from "@/drizzle/schema";
+
+export function createUserSubscription(
+  data: typeof UserSubscriptionTable.$inferInsert
+) {
+  return db.insert(UserSubscriptionTable).values(data).onConflictDoNothing({
+    target: UserSubscriptionTable.clerkUserId,
+  });
+}
